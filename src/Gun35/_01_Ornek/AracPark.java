d5aee5a1f57602ab94b864a9e3e67b9e601d01b7
ErtugrulGokcek <61143247+ErtugrulGokcek@users.noreply.github.com>
2022-07-12 18:08:33 +0300

    ekle

--- src/Gun35/_01_Ornek/AracPark.java
+++ src/Gun35/_01_Ornek/AracPark.java
@@ -1,4 +1,9 @@
 package Gun35._01_Ornek;
 
 public class AracPark {
+    public static void main(String[] args) {
+        Otobus otobus1=new Otobus("Beyaz",4500, "temsa", 45);
+        System.out.println("otobus1 = " + otobus1);
+
+    }
 }
